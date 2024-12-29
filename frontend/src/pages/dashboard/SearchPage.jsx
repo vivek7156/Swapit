1668c4875820f30d1dc9a224fc88924726b9f51b
“Vivek <viveksingh7156@gmail.com>
2024-12-29 21:26:19 +0530

    mistake2

--- frontend/src/pages/dashboard/SearchPage.jsx
+++ frontend/src/pages/dashboard/SearchPage.jsx
@@ -4,7 +4,7 @@ import { Search, Package, Heart } from 'lucide-react';
 import Navbar from '../../components/Navbar';
 import { useQuery, QueryClient, useQueryClient } from '@tanstack/react-query';
 import LoadingSpinner from '../../components/LoadingSpinner';
-import SearchableDropdown from '../../components/ui/searchableDropdown';
+import SearchableDropdown from '../../components/ui/searchableDropdown.jsx';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
