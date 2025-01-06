db13633d53c209ed0e7fc592fb72bae3bab41687
Vivek Singh <142485319+vivek7156@users.noreply.github.com>
2025-01-06 18:25:45 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-const SOCKET_URL = 'https://swapit-vhlk.onrender.com/'; 
+const SOCKET_URL = 'https://swapit-vhlk.onrender.com'; 
 
 const socket = io(SOCKET_URL, {
     withCredentials: true, // Important for cookies/session if used
